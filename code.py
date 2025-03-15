e8f641fe038a6fc3385b694bb8da3d138c3aace7
Sai-Manasvi <g298man@gmail.com>
2025-03-15 16:19:40 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,112 @@
+import torch
+from torch import nn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+url="https://raw.githubusercontent.com/Sai-Manasvi/stock-prediction-using-lstm/refs/heads/main/goldstock.csv"
+df = pd.read_csv(url)
+df.drop(['Unnamed: 0'], axis=1, inplace = True)
+
+stock_data = df
+stock_data['Date'] = pd.to_datetime(df.Date)
+print("Information about the dataset", end = "\n")
+print(stock_data.info()) 
+
+from sklearn.preprocessing import MinMaxScaler
+price = stock_data[['High','Low','Open','Close']]
+print(price[:5])
+
+scaler = MinMaxScaler(feature_range=(-1, 1))
+price = scaler.fit_transform(price)
+print(price[:5])
+
+train_window = 11
+import numpy as np
+def create_in_out_sequences(price, tw):
+    inout_seq = []
+    L = len(price)
+    print('Length = ', L)
+    
+    for i in range(L-tw):
+        data_seq = price[i:i+tw]
+        data_label = price[i+tw:i+tw+1][0][3]
+        inout_seq.append((data_seq ,data_label))
+    
+    data = inout_seq;
+    print('size of data : ', len(data))
+    
+    test_set_size = 500
+    train_set_size = len(data) - (test_set_size);
+    print('size of test : ', test_set_size)
+    print('size of train : ', train_set_size)
+    
+    train = data[:train_set_size]
+    test = data[train_set_size:]
+    train = train[:-(tw-1)]
+    return train, test
+    
+train,test = create_in_out_sequences(price, train_window)
+
+class LSTM(nn.Module):
+    def __init__(self, input_size=4, hidden_layer_size=100, output_size=1):
+        super().__init__()
+        self.hidden_layer_size = hidden_layer_size
+
+        self.lstm = nn.LSTM(input_size, hidden_layer_size)
+
+        self.linear = nn.Linear(hidden_layer_size, output_size)
+
+    def forward(self, input_seq):
+        hidden_cell = (torch.zeros(1,1,self.hidden_layer_size),
+                       torch.zeros(1,1,self.hidden_layer_size))
+        lstm_out, hidden_cell = self.lstm(input_seq.view(len(input_seq), 1, -1), hidden_cell)
+        predictions = self.linear(lstm_out.view(len(input_seq), -1))
+        return predictions[-1]
+      
+model = LSTM()
+loss_function = nn.MSELoss()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+model.eval()
+actual = []
+pred = []
+
+for seq, labels in test:
+    seq = torch.from_numpy(np.array(seq)).type(torch.FloatTensor)
+    labels = torch.from_numpy(np.array(labels)).type(torch.FloatTensor)
+    actual.append(labels.item())
+    with torch.no_grad():
+        pred.append(model(seq).item())
+
+actual = np.array(actual)
+pred = np.array(pred)
+pred = torch.from_numpy(np.array(pred)).type(torch.Tensor)
+actual = torch.from_numpy(np.array(actual)).type(torch.Tensor)
+
+import numpy as np
+pred_new = scaler.inverse_transform(np.c_[np.zeros(500), np.zeros(500), np.zeros(500), np.array(pred)])
+print(pred_new[:, 3])
+
+actual_new = scaler.inverse_transform(np.c_[np.zeros(500),np.zeros(500),np.zeros(500),np.array(actual)])
+print(actual_new[:,3])
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+mae = mean_absolute_error(actual_new, pred_new)
+print("Mean Absolute Error (MAE):", mae)
+
+rmse = np.sqrt(mean_squared_error(actual_new, pred_new))
+print("Root Mean Squared Error (RMSE):", rmse)
+fig, ax = plt.subplots()
+
+ax.plot(actual_new[:,3], 'r-', label='Actual')
+ax.plot(pred_new[:,3], 'c-', label='Predicted')
+
+ax.set_ylabel('Stock Value (dollars)')
+
+ax.set_ylim(min(min(actual_new[:,3]), min(pred_new[:,3])) - 5, max(max(actual_new[:,3]), max(pred_new[:,3])) + 5)
+plt.xticks([])
+
+ax.legend(loc='upper left')
+
+plt.show()
